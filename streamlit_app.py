0eb39847c2bd8d4aa1fd6dbdae2c76f45aa258cb
wk012 <126995690+wk012@users.noreply.github.com>
2023-03-04 18:39:24 -0700

    Update streamlit_app.py
    Added Emjois

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,11 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebaerry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free -Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Bluebaerry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free -Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
 
