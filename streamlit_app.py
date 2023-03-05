61760f1638a1bd1517dfc701d20af101901664a2
wk012 <126995690+wk012@users.noreply.github.com>
2023-03-04 18:31:24 -0700

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+stramlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 & Bluebaerry Oatmeal')
+streamlit.text('Kale. Spinach & Rocket Smoothie')
+streamit.text(('Hard-Boiled Free -Range Egg')
+
+
+
+
+
