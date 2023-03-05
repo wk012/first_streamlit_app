c737a0774ffbcebe59d05a00778fa300c7c52e1c
wk012 <126995690+wk012@users.noreply.github.com>
2023-03-04 18:34:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Bluebaerry Oatmeal')
-streamlit.text('Kale. Spinach & Rocket Smoothie')
+streamlit.text('Omega 3 & Bluebaerry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free -Range Egg')
 
 
